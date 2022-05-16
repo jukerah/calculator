0126c3199396e7ccaca9d2c2f770e81acb0047bc
Mario Elvio <juka_mebaj@hotmail.com>
2022-05-16 03:51:57 -0300

    project almost finished, just need to adjust the subtraction function

--- src/js/index.js
+++ src/js/index.js
@@ -26,55 +26,62 @@ const division = (numA, numB) => {
     result = numA / numB;
 }
 
-const showDisplay = (value) => {
+const nextCalc = (numA, numB, operator) => {
+    switch (operator) {
+        case '+':
+            sum(numA, numB);
+            break;
+        case '-':
+            subtraction(numA, numB);
+            break;
+        case '*':
+            multiplication(numA, numB);
+            break;
+        case '/':
+            division(numA, numB);
+            break;
+    }
 
+    displayNumCurrent.innerHTML = result.toString();
+}
+
+const showDisplay = (operator) => {
+    numA = parseFloat(displayNumCurrent.innerHTML);
+    displayOperator.innerHTML = operator;
+    displayNumOld.innerHTML = displayNumCurrent.innerHTML;
+    displayNumCurrent.innerHTML = '';
+}
+
+const validate = (keyPress) => {
+    switch (keyPress) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            if (displayNumOld.innerHTML === '' && displayNumCurrent.innerHTML === '' || displayNumOld.innerHTML !== '' && displayNumCurrent.innerHTML === '' && displayOperator.innerHTML !== '') {
+                break;
+            }
+            if (displayNumOld.innerHTML !== '' && displayNumCurrent.innerHTML !== '' || displayOperator.innerHTML !== '') {
+                numB = parseFloat(displayNumCurrent.innerHTML);
+                nextCalc(numA, numB, operator);
+                result = 0;
+            }
+            operator = keyPress;
+
+            break;
+    }
 }
 
 const handleClick = (keyPress) => {
 
     switch (keyPress) {
         case '+':
-            if (displayNumOld.innerHTML === '' && displayNumCurrent.innerHTML === '' && displayOperator.innerHTML) {
-                numA = parseFloat(displayNumCurrent.innerHTML);
-            }
-            if (displayNumOld.innerHTML === '' && displayNumCurrent.innerHTML === '' || displayNumOld.innerHTML !== '' && displayNumCurrent.innerHTML === '' && displayOperator.innerHTML !== '') {
-                break;
-            } else  {
-                numB = parseFloat(displayNumCurrent.innerHTML);
-                sum(numA, numB);
-                displayNumCurrent.innerHTML = result.toString();
-                result = 0;
-            }
-            
-            operator = keyPress;
-            numA = parseFloat(displayNumCurrent.innerHTML);
-            displayOperator.innerHTML = operator;
-            displayNumOld.innerHTML = numA;
-            displayNumCurrent.innerHTML = '';
-
-            break;
         case '-':
-            if (displayNumOld.innerHTML === '' && displayNumCurrent.innerHTML === '' || displayNumOld.innerHTML !== '' && displayNumCurrent.innerHTML === '' && displayOperator.innerHTML === '-') {
-                break;
-            } else  {
-                numB = parseFloat(displayNumCurrent.innerHTML);
-                subtraction(numA, numB);
-                displayNumCurrent.innerHTML = result.toString();
-                result = 0;
-            }
-            
-            operator = keyPress;
-            console.log(numA);
-            numA = parseFloat(displayNumCurrent.innerHTML);
-            console.log(numA);
-            displayOperator.innerHTML = operator;
-            displayNumOld.innerHTML = numA;
-            displayNumCurrent.innerHTML = '';
-
-            break;
         case '*':
-            break;
         case '/':
+            validate(keyPress)
+            showDisplay(operator);
+
             break;
         default:
             displayNumCurrent.innerHTML += keyPress;
@@ -91,36 +98,36 @@ const cleanDisplay = () => {
     displayNumCurrent.innerHTML = '';
 }
 
+
 const calculate = () => {
     numA = (displayNumOld.innerHTML === '') ? 0 : parseFloat(displayNumOld.innerHTML);
     numB = (displayNumCurrent.innerHTML === '') ? 0 : parseFloat(displayNumCurrent.innerHTML);
-    console.log(numA);
-    console.log(numB);
 
     switch (operator) {
         case '+':
             sum(numA, numB);
-            numA = 0;
-            numB = 0;
-            displayNumOld.innerHTML = '';
-            displayOperator.innerHTML = '';
-            displayNumCurrent.innerHTML = parseFloat(result);
-            result = 0;
-
             break;
         case '-':
             subtraction(numA, numB);
+            break;
+        case '*':
+            multiplication(numA, numB);
+            break;
+        case '/':
+            division(numA, numB);
+            break;
+    }
+    switch (operator) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
             numA = 0;
             numB = 0;
             displayNumOld.innerHTML = '';
             displayOperator.innerHTML = '';
             displayNumCurrent.innerHTML = parseFloat(result);
             result = 0;
-
-            break;
-        case '*':
-            break;
-        case '/':
             break;
     }
 }
